f46cebf78f70f7409814c780e4e6bc8799754c43
Laith Albusairi <laithalbusairi@gmail.com>
2023-01-04 22:15:03 -0800

    added to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,19 @@ import './Navbar.css'
 
 const Navbar = () => {
 const [click, setClick] =useState(false)
+
 const handleClick = () => setClick(!click)
 
   return (
     <div className='header'>
             <nav className='navbar'>
-                {/* <a href='/' className='logo'>
+                <a href='/' className='logo'>
                     <img src={logo} alt='logo'/>
-                </a> */}
+                </a>
                 <div classname='hamburger' onClick={handleClick}>
                     {click ? (<FaTimes size={30} style={{color: '#fffff'}}/>) :
                     (<FaBars size={30} style={{color: '#ffffff'}}/> )}
+
                 </div>
                     <ul className={click ? "nav-menu active" : "nav-menu"}>
                         <li className='nav-item'>
