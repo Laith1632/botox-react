48781dc0d6bfb09295554375a4257132160314ae
Laith Albusairi <laithalbusairi@gmail.com>
2023-01-04 20:17:22 -0800

    added navbar titles and image need to change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,9 @@
 import React from 'react'
-// import logo from './images/logo'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import logo from './images/logo.jpeg'
+import './Navbar.css'
+
+
 
 const Navbar = () => {
   return (
@@ -9,8 +13,22 @@ const Navbar = () => {
                     <img src={logo} alt='logo'/>
                 </a>
                 <div classname='hamburger'>
-                    
+                    <FaBars/>
                 </div>
+                    <ul className='nav-menu'>
+                        <li className='nav-item'>
+                            <a href='/'>Home</a>
+                        </li>
+
+                        <li className='nav-item'>
+                            <a href='/'>About</a>
+                        </li>
+
+                        <li className='nav-item'>
+                            <a href='/'>Pricing</a>
+                        </li>
+
+                    </ul>
             </nav>
 
     </div>
