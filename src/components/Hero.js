417b074f83c291d7a2e1fceed3b9ddff0270b3b7
Laith Albusairi <laithalbusairi@gmail.com>
2023-01-05 00:23:19 -0800

    added to hero text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,9 +9,8 @@ const Hero = () => {
                 <p>Beautification and Medical Aesthetics</p>
                 <p>509-595-3154 (Text/Call)</p>
                 <p>Email: MedAsYou@yahoo.com</p>
-                <p>1717 E. Cherokee St. #105 Springfield, Missouri 65804
-                    (Office located inside the Laser Studio)
-                </p>
+                <p>1717 E. Cherokee St. #105 Springfield, Missouri 65804</p>
+                <p> (Office located inside the Laser Studio)</p>
                 <button href='/' className='button'>Free Consultation</button>
             </div>
         </div>
