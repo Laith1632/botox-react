1941b270ca517b3ff3e7c1ac825982581accc26a
Laith Albusairi <laithalbusairi@gmail.com>
2023-01-04 19:58:19 -0800

    started navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from './images/logo'
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+            <nav className='navbar'>
+                <a href='/' className='logo'>
+
+                </a>
+            </nav>
+
+    </div>
+  )
+}
+
+export default Navbar
