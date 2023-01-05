85461ded23e991c5deb41dbdbbcdfa1935f000fc
Laith Albusairi <laithalbusairi@gmail.com>
2023-01-04 23:17:53 -0800

    added styling to about p[age

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import john from './images/john-doe.png'
+// import lisa from './images/john-doe.png'
 import './About.css'
 
 const About = () => {
     return (
         <div className='about' id='about'>
             <div className='container'>
-                <img src={john} alt='john' />
+                {/* <img src={lisa} alt='john' /> */}
                 <div className='col-2'>
                     <h2>About</h2>
                     <span className='line'></span>
