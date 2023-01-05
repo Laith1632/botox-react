5dfe785ba7714fe1f0a27f9b67584cac30a65d4f
Laith Albusairi <laithalbusairi@gmail.com>
2023-01-04 20:01:15 -0800

    added hamburger to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import logo from './images/logo'
+// import logo from './images/logo'
 
 const Navbar = () => {
   return (
     <div className='header'>
             <nav className='navbar'>
                 <a href='/' className='logo'>
-
+                    <img src={logo} alt='logo'/>
                 </a>
+                <div classname='hamburger'>
+                    
+                </div>
             </nav>
 
     </div>
