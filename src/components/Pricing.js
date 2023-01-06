94770c193d40bc4e1dcacbf037154e3d4297a3f9
Laith Albusairi <laithalbusairi@gmail.com>
2023-01-06 11:56:17 -0800

    pricing page typed

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,28 +1,29 @@
 import React from 'react'
+import './Pricing.css'
+
 
 const Pricing = () => {
   return (
     <div>
         <div>
-          <div>
-            <div className='container 1'>
-                <p>Xeomin $8/unit </p>
-                <p>blahbalalhalhalhlah alhlahlhdslhal lhdalhdlhalhdla lhadhladljladj</p>
+          <div className='containerpricing'>
+            <div className='container1'>
+             <p>Xeomin $8/unit </p>
+             <p>blahbalalhalhalhlah alhlahlhdslhal lhdalhdlhalhdla lhadhladljladj</p>
 
             </div>
-            <div className='container 2'>
-
-
+            <div className='container2'>
+             <p>Juvederm Ulta $520/ syringe</p>
+             <p> blah blah bnandkcknsdkacndsknckdsanc</p>
             </div>
-            <div className='container 3'>
-
-
+            <div className='container3'>
+             <p>Belotero Balance (lips) $335/syringe</p>
+             <p>andnacjncdjncjdsncjdsncjdsncdsjncdsjcnscn</p>
             </div>
-            <div className='container 4'>
-
+            <div className='container4'>
+             <p>Revanesse Versa $400/syringe</p>
+             <p> sjdfvnjdfnvjfdnvjvnfdjvnfdjvnfdjvnfdjnvjdfnvvdfdnjvnfdjvnfdjvn</p>
                   
-                
-             
             </div>
           </div>
          </div>
