0a3231ad689cb98b54b0f6d43642de5ad3f31f2b
Laith Albusairi <laithalbusairi@gmail.com>
2023-01-16 21:49:05 -0800

    added cards

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -4,31 +4,155 @@ import './Pricing.css'
 
 const Pricing = () => {
   return (
-    <div>
-        <div>
-         
-          <div className='gridcontainer'>
-            <div className='container1'>
-             <p>Xeomin $8/unit </p>
-             <p>blahbalalhalhalhlah alhlahlhdslhal lhdalhdlhalhdla lhadhladljladj</p>
-            </div>
-            <div className='container2'>
-             <p>Juvederm Ulta $520/ syringe</p>
-             <p> blah blah bnandkcknsdkacndsknckdsanc</p>
-            </div>
-            <div className='container3'>
-             <p>Belotero Balance (lips) $335/syringe</p>
-             <p>andnacjncdjncjdsncjdsncjdsncdsjncdsjcnscn</p>
-            </div>
-            <div className='container4'>
-             <p>Revanesse Versa $400/syringe</p>
-             <p> sjdfvnjdfnvjfdnvjvnfdjvnfdjvnfdjvnfdjnvjdfnvvdfdnjvnfdjvnfdjvn</p>
+
+<div className='all-card-container'> 
+   <div className='card-container'>
+          <div className='product-title'>
+            <h2>Xeomin</h2>
           </div>
-            
+          <div className='product-price'>
+            <h3>$8/unit</h3>
           </div>
-         </div>
+          <div className='benafits-list'>
+          <ul >
+                        <li>
+                            <p>good skin</p>
+                        </li>
+                            <p>good health</p>
+                        <li>
+                            <p>gog goog </p>
+                        </li>
+                            <p>annananna</p>
+                        <li>
+                           <p>banananananan</p>
+                        </li>
+            </ul>
+          </div>
+    
+    
+    
     </div>
+
+    <div className='card-container'>
+          <div className='product-title'>
+            <h2>Juvederm Ulta</h2>
+          </div>
+          <div className='product-price'>
+            <h3>$520/ syringe</h3>
+          </div>
+          <div className='benafits-list'>
+          <ul >
+                        <li>
+                            <p>good skin</p>
+                        </li>
+                            <p>good health</p>
+                        <li>
+                            <p>gog goog </p>
+                        </li>
+                            <p>annananna</p>
+                        <li>
+                           <p>banananananan</p>
+                        </li>
+            </ul>
+          </div>
+    
+    
+    
+    </div>
+
+    <div className='card-container'>
+          <div className='product-title'>
+            <h2>Belotero Balance (lips)</h2>
+          </div>
+          <div className='product-price'>
+            <h3>$335/syringe</h3>
+          </div>
+          <div className='benafits-list'>
+          <ul >
+                        <li>
+                            <p>good skin</p>
+                        </li>
+                            <p>good health</p>
+                        <li>
+                            <p>gog goog </p>
+                        </li>
+                            <p>annananna</p>
+                        <li>
+                           <p>banananananan</p>
+                        </li>
+            </ul>
+          </div>
+    
+    
+    
+    </div>
+
+
+    <div className='card-container'>
+          <div className='product-title'>
+            <h2>Revanesse Versa</h2>
+          </div>
+          <div className='product-price'>
+            <h3>$400/syringe</h3>
+          </div>
+          <div className='benafits-list'>
+          <ul >
+                        <li>
+                            <p>good skin</p>
+                        </li>
+                            <p>good health</p>
+                        <li>
+                            <p>gog goog </p>
+                        </li>
+                            <p>annananna</p>
+                        <li>
+                           <p>banananananan</p>
+                        </li>
+            </ul>
+          </div>
+    
+    
+    
+    </div>
+
+</div>  
+
+
   )
-}
+  }
 
 export default Pricing
+
+
+
+
+
+
+
+
+
+
+{/* <div className='wrapper'>
+         
+<div className='gridcontainer'>
+  <div className='container1'>
+   <p>Xeomin $8/unit </p>
+   <p>blahbalalhalhalhlah alhlahlhdslhal lhdalhdlhalhdla lhadhladljladj</p>
+  </div>
+  <div className='container2'>
+   <p>Juvederm Ulta $520/ syringe</p>
+   <p> blah blah bnandkcknsdkacndsknckdsanc</p>
+  </div>
+  <div className='container3'>
+   <p>Belotero Balance (lips) $335/syringe</p>
+   <p>andnacjncdjncjdsncjdsncjdsncdsjncdsjcnscn</p>
+  </div>
+  <div className='container4'>
+   <p>Revanesse Versa $400/syringe</p>
+   <p> sjdfvnjdfnvjfdnvjvnfdjvnfdjvnfdjvnfdjnvjdfnvvdfdnjvnfdjvnfdjvn</p>
+</div>
+  
+</div>
+</div>
+)
+} */}
